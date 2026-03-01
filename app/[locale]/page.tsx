656c5061e0cb236d9bf276d75baf3de1917a2a85
hazem172002 <hazmj05@gmail.com>
2026-03-01 11:05:04 +0200

    core: finalize next-intl

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -6,9 +6,7 @@ export default function HomePage() {
   const t = useTranslations("HomePage");
 
   return (
-    <main className="flex min-h-screen items-center justify-center bg-off-white px-6">
       <h1 className="text-4xl font-bold text-write-main">{t("title")}</h1>
-    </main>
   );
 }
 
